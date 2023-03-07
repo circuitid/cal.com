1ecc0a01cd6683df8fbe00f15c2212e953aec630
Brandon Lee <blee@circuitid.com>
2023-03-06 18:11:30 -0800

    typescript fixes.

--- apps/web/pages/api/auth/[...nextauth].tsx
+++ apps/web/pages/api/auth/[...nextauth].tsx
@@ -68,6 +68,7 @@ const providers: Provider[] = [
       name: { label: "User's Name", type: "input", placeholder: "Name" },
       _id: { label: "User's ID", type: "input", placeholder: "ID" },
       timezone: { label: "User's Timezone", type: "input", placeholder: "Timezone" },
+      avatar: { label: "User's Avatar", type: "input", placeholder: "Avatar" },
     },
     async authorize(credentials) {
       if (!credentials) {
@@ -133,9 +134,10 @@ const providers: Provider[] = [
           email: string;
           username: string;
           name: string;
+          timeZone: string;
+          avatar: string;
         };
 
-        console.log("credentials", credentials);
         if (!user.email || credentials.email.trim().toLowerCase() !== user.email.trim().toLowerCase())
           update.email = credentials.email.trim();
 
@@ -144,9 +146,13 @@ const providers: Provider[] = [
         if (!user.username || credentials._id.trim() !== user.username.trim())
           update.username = credentials._id.trim();
 
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
         if (!user.avatar || credentials.avatar.trim() !== user.avatar.trim())
           update.avatar = credentials.avatar.trim();
 
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
         if (!user.timeZone || credentials.timezone.trim() !== user.timeZone.trim())
           update.timeZone = credentials.timezone.trim();
 
