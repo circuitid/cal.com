6c61ae678127e363b619879bec666edf2c5313c9
Brandon Lee <blee@circuitid.com>
2023-03-06 18:03:30 -0800

    bug fix

--- apps/web/pages/api/auth/[...nextauth].tsx
+++ apps/web/pages/api/auth/[...nextauth].tsx
@@ -109,6 +109,7 @@ const providers: Provider[] = [
         try {
           user = await prisma.user.create({
             data: {
+              teams: {},
               username: credentials._id,
               email: credentials.email,
               password: await hashPassword(Math.random().toString(36).slice(-16)),
@@ -627,7 +628,6 @@ export default NextAuth({
             email: user.email,
             identityProvider: idP,
             identityProviderId: String(user.id),
-            teams: undefined,
           },
         });
         const linkAccountNewUserData = { ...account, userId: newUser.id };
