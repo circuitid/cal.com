d27d90c3cb314c78850765aef01f06ff65a32702
Brandon Lee <blee@circuitid.com>
2023-03-16 00:39:37 -0700

    kustomize

--- packages/app-store/circuitidmeetvideo/lib/VideoApiAdapter.ts
+++ packages/app-store/circuitidmeetvideo/lib/VideoApiAdapter.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
+import prisma from "@calcom/prisma";
 import type { CalendarEvent } from "@calcom/types/Calendar";
 import type { PartialReference } from "@calcom/types/EventManager";
 import type { VideoApiAdapter, VideoCallData } from "@calcom/types/VideoApiAdapter";
@@ -10,13 +11,26 @@ const CircuitIDMeetApiAdapter = (): VideoApiAdapter => {
       return Promise.resolve([]);
     },
     createMeeting: async (event: CalendarEvent): Promise<VideoCallData> => {
-      console.log("scheduling event", event);
+      console.error("scheduling event", event);
+
+      if (!event || !event.destinationCalendar || !event.destinationCalendar.userId)
+        return Promise.reject("Organizer ID not found");
+
+      const user = await prisma.user.findUnique({
+        where: { id: event.destinationCalendar.userId },
+        select: {
+          username: true,
+        },
+      });
+
+      if (!user || !user.username) return Promise.reject("Organizer not found");
+
       const meetingID = uuidv4();
       return Promise.resolve({
         type: "circuitidmeet_video",
         id: meetingID,
         password: "",
-        url: "https://meet.circuitid.com/",
+        url: "https://meet.circuitid.com/" + user.username,
       });
     },
     deleteMeeting: async (): Promise<void> => {
